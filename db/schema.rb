54d6ddef4b004f8c0c74c3d7b44f47f46841fc58
sapatel93 <sapatel93@yahoo.com>
2016-04-21 18:23:20 +0000

    set show users page on admin login only, user's name can be add from sign up page.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160415204421) do
+ActiveRecord::Schema.define(version: 20160421170334) do
 
   create_table "events", force: :cascade do |t|
     t.string   "name",        limit: 255
