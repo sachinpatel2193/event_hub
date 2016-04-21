8eb1b058c5558a848de5c1a7f63a7baca6676170
jaspreet93 <erjaspreetkaur.20@gmail.com>
2016-04-21 23:26:24 +0000

    login page and css

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :users
   
   root 'events#index'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
