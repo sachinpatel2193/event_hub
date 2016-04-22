a1d6dee09ecb000e7de143fa38919fb4e6b05e37
sapatel93 <sapatel93@yahoo.com>
2016-04-22 15:04:31 +0000

    when user creates event, it will store userid of the user who created event i.e. one to many relationship between user and event

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, only: [:index]
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   # GET /users
@@ -10,6 +11,7 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
+    @user = User.find(params[:id])
   end
 
   # GET /users/new
@@ -19,6 +21,7 @@ class UsersController < ApplicationController
 
   # GET /users/1/edit
   def edit
+    @user = User.find(params[:id])
   end
 
   # POST /users
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160421183617) do
+ActiveRecord::Schema.define(version: 20160421221357) do
 
   create_table "events", force: :cascade do |t|
     t.string   "name",        limit: 255
@@ -25,7 +25,10 @@ ActiveRecord::Schema.define(version: 20160421183617) do
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
     t.boolean  "status"
+    t.integer  "user_id",     limit: 4
   end
+
+  add_index "events", ["user_id"], name: "index_events_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "name",                   limit: 255
@@ -47,4 +50,5 @@ ActiveRecord::Schema.define(version: 20160421183617) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "events", "users"
 end
