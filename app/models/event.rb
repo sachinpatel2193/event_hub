0d776cae972ad1e39bd0ec06119dcf59e7df87b8
sapatel93 <sapatel93@yahoo.com>
2016-04-23 16:05:03 +0000

    set css properly, events creation, ticket booking properly, css on login, sign uppage is remaining.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,7 @@
 class Event < ActiveRecord::Base
     has_and_belongs_to_many :users
-    has_many :users, :through: :event_users
+    has_many :users
+    has_many :event_users
    
     validates :name, presence: true
     
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
-  has_many :events, through: :event_users
+  has_many :events
+  has_many:event_users
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
