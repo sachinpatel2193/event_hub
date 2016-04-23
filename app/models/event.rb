22e91d8af1810ff20499510095c2b16d528a3c1c
truptipatel1490 <truptipatel1490@gmail.com>
2016-04-23 03:10:33 +0000

    change the abuot page and change the style

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,17 @@
 class Event < ActiveRecord::Base
     belongs_to :user
+
+    validates :name, presence: true
     
-end
+    validates :address, presence: true
+    
+    validates :city, presence: true
+    
+    validates :province, presence: true
+    
+    zip_canada = /\A[a-zA-Z]{1}\d{1}[a-zA-Z]{1}[ -]?\d{1}[a-zA-Z]{1}\d{1}\z/
+    validates :postal_code, :presence => true, :format => { :with => zip_canada }
+      
+    validates :country, presence: true
+    
+end    
